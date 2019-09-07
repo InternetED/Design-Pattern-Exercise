751758affff56914085322a62eedb07bd5225de9
Ed <salahayo3192@gmail.com>
2019-09-07 20:07:27 +0800

    1.Add examples of factory patterns 2.Add examples of builder patterns

--- src/main/kotlin/create/factorymethod/Computer.kt
+++ src/main/kotlin/create/factorymethod/Computer.kt
@@ -1,10 +1,10 @@
-package create.abstrtact.factory.Impl
+package create.factory
 
 /**
  *@data: 2019/1/14 : 下午 10:32
  *@author: Ed
  *@email: salahayo3192@gmail.com
  **/
-interface IComputer {
-    fun getName(): String
+interface Computer {
+    fun getSuppliersName(): String
 }
---
+++ src/main/kotlin/create/factorymethod/Impl/computer/AsusComputer.kt
@@ -0,0 +1,13 @@
+package create.factory.Impl.computer
+
+import create.factory.Computer
+
+
+/**
+ *@data: 2019/1/14 : 下午 10:33
+ *@author: Ed
+ *@email: salahayo3192@gmail.com
+ **/
+class AsusComputer : Computer {
+    override fun getSuppliersName(): String = "Asus"
+}
